a3c8950114e5fe0594ce3481a73980aa116df36a
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-07-25 18:05:13 -0700

    finished list command

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import typer, os, json
 from rich import print
+from rich.table import Table
 from datetime import datetime
 
 # initialize config
 app = typer.Typer()
+config_path = f"{os.path.expanduser('~/.templater')}"
 
 
 # cli commands
 @app.command()
-def view():
+def list():
   """
-  View current current templates
+  List current current templates
   """
-  raise NotImplementedError
+  config = load()
+  table = Table("", "Name", "Created", show_header=True, header_style="bold blue")
+  for i, template in enumerate(config["templates"]):
+    table.add_row(str(i), template["name"], template["created"])
+  print(table)
+
 
 @app.command()
-def add(name: str, path: str):
+def add(name: str = typer.Argument(..., help="template name"), path: str = typer.Argument(..., help="path to file/directory")):
   """
   Add a new template
   """
@@ -27,8 +34,6 @@ def add(name: str, path: str):
   config["templates"].append(template)
   save(config)
   print(f"[bold green] Added template {name}")
-
-
 
 @app.command()
 def delete():
@@ -43,8 +48,9 @@ def load():
   """
   Loads the config file
   """
-  if os.path.exists('~/.templater/config.json'):
-    with open('~/.templater/config.json', 'r') as f:
+  path = os.path.join(config_path, 'config.json')
+  if os.path.exists(path):
+    with open(path, 'r') as f:
       return json.load(f)
   else:
     return {"templates": []}
@@ -53,7 +59,9 @@ def save(config):
   """
   Saves the config file
   """
-  with open('~/.templater/config.json', 'w') as f:
+  os.makedirs(config_path, exist_ok=True)
+  path = os.path.join(config_path, 'config.json')
+  with open(path, 'w') as f:
     json.dump(config, f)
 
 
