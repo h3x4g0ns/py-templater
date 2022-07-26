7b3743f975ffded7b55877078b82ee42467db1ac
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-07-25 17:39:10 -0700

    loading + saving config

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import typer
+import typer, os, json
 from rich import print
+from datetime import datetime
 
+# initialize config
 app = typer.Typer()
 
+
+# cli commands
 @app.command()
 def view():
   """
@@ -11,11 +15,20 @@ def view():
   raise NotImplementedError
 
 @app.command()
-def add():
+def add(name: str, path: str):
   """
   Add a new template
   """
-  raise NotImplementedError
+  config = load()
+  template = {
+    "name": name,
+    "created": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  }
+  config["templates"].append(template)
+  save(config)
+  print(f"[bold green] Added template {name}")
+
+
 
 @app.command()
 def delete():
@@ -23,6 +36,26 @@ def delete():
   Delete a template
   """
   raise NotImplementedError
+
+
+# utility functions
+def load():
+  """
+  Loads the config file
+  """
+  if os.path.exists('~/.templater/config.json'):
+    with open('~/.templater/config.json', 'r') as f:
+      return json.load(f)
+  else:
+    return {"templates": []}
+
+def save(config):
+  """
+  Saves the config file
+  """
+  with open('~/.templater/config.json', 'w') as f:
+    json.dump(config, f)
+
 
 if __name__ == "__main__":
   app()
